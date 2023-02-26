07b4d5b142ae0918024d8c3609df39d5c1f68aec
Jarret de Jong <jardejong111@gmail.com>
2023-02-26 17:33:48 -0600

    test commit

--- Assets/ClawBehavior.cs
+++ Assets/ClawBehavior.cs
@@ -4,23 +4,66 @@ using UnityEngine;
 
 public class ClawBehavior : MonoBehaviour
 {
-    public GameObject claw;
-    public KeyCode dropClaw = KeyCode.S;
+    public GameObject boat;
 
-    public Vector3 dropPos;
+    public float speed, startWaitTime;
+    private float waitTime;
+
+    public Transform[] moveSpots;
+
+    private bool clawMove = false;
+    private bool intialMov = true;
+    private bool nextMov = false;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        waitTime = startWaitTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(dropClaw)) {
-            Instantiate(claw, transform.position, Quaternion.identity);
+        if (clawMove) {
+            if (intialMov) {
+                transform.position = Vector2.MoveTowards(transform.position, moveSpots[1].position, speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, moveSpots[1].position) < 0.2f) {
+                    if (waitTime <= 0) {
+                        waitTime = startWaitTime;
+                        intialMov = false;
+                        nextMov = true;
+                    }
+                    else {
+                        waitTime -= Time.deltaTime;
+                    }
+                }
+            }
+            if (nextMov) {
+                transform.position = Vector2.MoveTowards(transform.position, moveSpots[0].position, speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, moveSpots[0].position) < 0.2f) {
+                    boat.GetComponent<PlayerMovement>().ToggleControl(true);
+                    clawMove = false;
+                    intialMov = true;
+                    nextMov = false;
+                }
+            }
+        }
+    }
+
+    public void Drop() {
+        clawMove = true; 
+    }
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.tag == "Prawn") {
+            Destroy(coll.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision coll) {
+        if (coll.gameObject.tag == "Prawn") {
+            Destroy(coll.gameObject);
         }
     }
 }
